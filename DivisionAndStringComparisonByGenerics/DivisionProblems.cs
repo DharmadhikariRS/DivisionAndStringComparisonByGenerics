7a20cf0d54770155f203a2cb2a4c1aaf840017a5
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-26 17:27:03 +0530

    [Rushikesh] Division And StringComparison By Generics

---
+++ DivisionAndStringComparisonByGenerics/DivisionProblems.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisionAndStringComparisonByGenerics
+{
+    internal class DivisionProblems
+    {
+        public void IntDivision()
+        {
+            Console.WriteLine("Enter 2 Integers");
+            int Num1 = int.Parse(Console.ReadLine());
+            int Num2 = int.Parse(Console.ReadLine());
+
+            float Div = ((float)Num1 / (float)Num2);
+
+            Console.WriteLine("Division is= " + Div);
+        }
+        public void FloatDivision()
+
+        {
+            Console.WriteLine("Enter 2 Float values");
+            float Num1 = float.Parse(Console.ReadLine());
+            float Num2 = float.Parse(Console.ReadLine());
+
+            float Div = (Num1 / Num2);
+
+            Console.WriteLine("Division is= " + Div);
+        }
+        public void CompareString()
+        {
+            Console.WriteLine("Enter 2 Strings");
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            s1 = s1.ToUpper();
+            s2 = s2.ToUpper();
+            if (s1 == s2)
+            {
+                Console.WriteLine("Strings are equal");
+            }
+            else
+            {
+                Console.WriteLine("Strings are not Equal");
+            }
+        }
+        public void DivByGenerics<T>(T Val1, T Val2)
+        {
+            if (Val1.GetType() == typeof(int))
+            {
+                float Div;
+
+                Div = (float)(dynamic)Val1 / (float)(dynamic)Val2;
+                Console.WriteLine("Division is= " + Div);
+            }
+            else
+            if (Val1.GetType() == typeof(float))
+            {
+                float Div;
+                Div = (dynamic)Val1 / (dynamic)Val2;
+                Console.WriteLine("Division is= " + Div);
+            }
+            else
+            {
+                string s1 = Val1.ToString().ToUpper();
+                string s2 = Val2.ToString().ToUpper();
+                if (s1.Equals(s2))
+                {
+                    Console.WriteLine("Strings are equal");
+                }
+                else
+                {
+                    Console.WriteLine("Strings are not Equal");
+                }
+            }
+        }
+
+    }
+}
---
+++ DivisionAndStringComparisonByGenerics/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace DivisionAndStringComparisonByGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q =Write interger number division , float number division  and compare the
+            //string must be in upper case only then refactor code by using generic method
+
+            Console.WriteLine("Press 1 for intiger Division without generics");
+            Console.WriteLine("Press 2 for float Division without generics");
+            Console.WriteLine("Press 3 for intiger Division with generics");
+            Console.WriteLine("Press 4 for float Division with generics");
+            Console.WriteLine("Press 5 for Strings compare without generics");
+            Console.WriteLine("Press 6 for Strings compare with generics");
+
+            DivisionProblems divisionProblems = new DivisionProblems();
+
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    divisionProblems.IntDivision();
+                    break;
+                case 2:
+                    divisionProblems.FloatDivision();
+                    break;
+                case 3:
+                    Console.WriteLine("Enter 2 Integers");
+                    int a = int.Parse(Console.ReadLine());
+                    int b = int.Parse(Console.ReadLine());
+
+                    divisionProblems.DivByGenerics(a, b);
+                    break;
+                case 4:
+
+                    Console.WriteLine("Enter 2 Float values");
+                    float Num1 = float.Parse(Console.ReadLine());
+                    float Num2 = float.Parse(Console.ReadLine());
+
+                    divisionProblems.DivByGenerics(Num1, Num2);
+
+                    break;
+                case 5:
+
+                    divisionProblems.CompareString();
+                    break;
+                case 6:
+                    Console.WriteLine("Enter 2 Strings");
+                    string Number1 = Console.ReadLine();
+                    string Number2 = Console.ReadLine();
+
+                    divisionProblems.DivByGenerics(Number1, Number2);
+                    break;
+            }
+        }
+    }
+}
